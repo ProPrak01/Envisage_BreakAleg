93f4e58067e6b831d4e6aeb925095e9c13f177bc
Prakash <127179011+ProPrak01@users.noreply.github.com>
2023-09-15 20:02:16 +0530

    change

--- Assets/Attacher.cs
+++ Assets/Attacher.cs
@@ -4,45 +4,81 @@ public class Attacher : MonoBehaviour
 {
     public string playerTag = "Player"; // Tag of the player GameObject.
     public GameObject objectPrefab; // The prefab to instantiate and make a child of the player.
-    public KeyCode attachKey = KeyCode.E; // The key to press to attach the object.
-
+    public float interactionDistance = 2.0f; // Distance to trigger interaction with the block.
+    public KeyCode attachKey = KeyCode.E; // The key to press to attach/detach the object.
+    public Material glowMaterial; // Material to apply when the player is near the block.
+    public Material originalMaterial;
     private GameObject instantiatedObject; // Reference to the instantiated object.
+    private bool isPlayerNear = false; // Flag to track player proximity.
+    private Renderer blockRenderer; // Reference to the block's renderer.
+
+    private void Start()
+    {
+        blockRenderer = GetComponentInChildren<Renderer>();
+        originalMaterial = blockRenderer.material;
+    }
+
+    private void Update()
+    {
+        // Check if the player is near the block.
+        if (isPlayerNear)
+        {
+            // Apply the glow material when the player is near.
+            blockRenderer.material = glowMaterial;
+
+            // Check if the player presses the attach key and no object is attached to the player.
+            if (Input.GetKeyDown(attachKey) && instantiatedObject == null)
+            {
+                // Find the player with the "Player" tag.
+                GameObject player = GameObject.FindGameObjectWithTag(playerTag);
+
+                // Check if a player object is found.
+                if (player != null)
+                {
+                    // Calculate the distance between the player and the block.
+                    float distance = Vector3.Distance(player.transform.position, transform.position);
+
+                    // Check if the player is within the interaction distance.
+                    if (distance <= interactionDistance)
+                    {
+                        // Instantiate the objectPrefab and make it a child of the player.
+                        instantiatedObject = Instantiate(objectPrefab, player.transform);
+
+                        instantiatedObject.transform.localPosition = new Vector3(0f, 3.2f, 0f); // Example position.
+                    }
+                }
+            }
+        }
+        else
+        {
+            // Reset the material when the player is not near.
+            blockRenderer.material = originalMaterial;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the colliding object has the "playerTag".
+        // Check if the colliding object has the "Player" tag.
         if (other.CompareTag(playerTag))
         {
-            // Store a reference to the instantiated object.
-            instantiatedObject = Instantiate(objectPrefab, transform.position, Quaternion.identity);
-
-            // Make the instantiatedObject a child of the player's transform.
-            instantiatedObject.transform.parent = other.transform;
+            // Check if the player already has an attached object.
+            if (other.transform.childCount == 2)
+            {
+                isPlayerNear = false; // Prevent attachment when an object is already attached.
+            }
+            else
+            {
+                isPlayerNear = true;
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        // Check if the colliding object has the "playerTag".
+        // Check if the colliding object has the "Player" tag.
         if (other.CompareTag(playerTag))
         {
-            // Detach the object if it was attached.
-            DetachObject();
+            isPlayerNear = false;
         }
-    }
-
-    private void Update()
-    {
-        // Check if the attachKey is pressed and the instantiatedObject is not null.
-        if (Input.GetKeyDown(attachKey) && instantiatedObject != null)
-        {
-            // Detach the object when the attach key is pressed.
-            DetachObject();
-        }
-    }
-
-    private void DetachObject()
-    {
-        // Remove the instantiatedObject from the player's children
     }
 }
---
+++ Assets/FloorTransferController.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+public class TransferBlockController : MonoBehaviour
+{
+    public string playerTag = "Player"; // Tag of the player GameObject.
+    public float interactionDistance = 2.0f; // Distance to trigger interaction with the block.
+    public Material glowMaterial; // Material to apply when the player is near the block.
+    public Material originalMaterial;
+
+    private bool isPlayerNear = false; // Flag to track player proximity.
+    private Renderer blockRenderer; // Reference to the block's renderer.
+
+    private void Start()
+    {
+        blockRenderer = GetComponent<Renderer>();
+        originalMaterial = blockRenderer.material;
+
+    }
+
+    private void Update()
+    {
+        // Check if the player is near the block.
+        if (isPlayerNear)
+        {
+            // Apply the glow material when the player is near.
+            blockRenderer.material = glowMaterial;
+
+            // Check if the player presses the attach key.
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // Find the player with the "Player" tag.
+                GameObject player = GameObject.FindGameObjectWithTag(playerTag);
+
+                // Check if a player object is found.
+                if (player != null)
+                {
+                    // Calculate the distance between the player and the block.
+                    float distance = Vector3.Distance(player.transform.position, transform.position);
+
+                    // Check if the player is within the interaction distance.
+                    if (distance <= interactionDistance)
+                    {
+                        // Transfer the child object from the player to the block.
+                        TransferChildObject(player, transform);
+                    }
+                }
+            }
+        }
+        else
+        {
+            // Reset the material when the player is not near.
+            blockRenderer.material = originalMaterial;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the colliding object has the "Player" tag.
+        if (other.CompareTag(playerTag))
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the colliding object has the "Player" tag.
+        if (other.CompareTag(playerTag))
+        {
+            isPlayerNear = false;
+        }
+    }
+
+    private void TransferChildObject(GameObject fromObject, Transform toObject)
+    {
+        // Check if the player already has a child object.
+        if (fromObject.transform.childCount == 2)
+        {
+            // Get the child object.
+            Transform child = fromObject.transform.GetChild(1);
+
+            // Make it a child of the block.
+            child.SetParent(toObject);
+
+            // Reset the position relative to the block.
+            child.localPosition = Vector3.zero;
+            child.localScale = new Vector3(1f, 1f, 1f);
+
+            // Rotate the child object by 45 degrees on the Y-axis.
+            child.localRotation = Quaternion.Euler(0f, 45f, 0f);
+        }
+    }
+}
