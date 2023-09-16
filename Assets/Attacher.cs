7effd760ad524386c0564dafc206704b10360994
Prakash <127179011+ProPrak01@users.noreply.github.com>
2023-09-16 09:29:42 +0530

    change

--- Assets/Attacher.cs
+++ Assets/Attacher.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Attacher : MonoBehaviour
 {
     public string playerTag = "Player"; // Tag of the player GameObject.
+    public string player2Tag = "Player2"; // Tag of the player GameObject.
+    public float p1p2;
     public GameObject objectPrefab; // The prefab to instantiate and make a child of the player.
     public float interactionDistance = 2.0f; // Distance to trigger interaction with the block.
     public KeyCode attachKey = KeyCode.E; // The key to press to attach/detach the object.
@@ -25,12 +27,24 @@ public class Attacher : MonoBehaviour
         {
             // Apply the glow material when the player is near.
             blockRenderer.material = glowMaterial;
+            GameObject player = null;
+            if (p1p2 == 1)
+            {
+                player = GameObject.FindGameObjectWithTag(playerTag);
+                attachKey = KeyCode.E;
+            }
+            else if (p1p2 == 2)
+            {
+                player = GameObject.FindGameObjectWithTag(player2Tag);
+                attachKey = KeyCode.O;
 
+
+            }
             // Check if the player presses the attach key and no object is attached to the player.
             if (Input.GetKeyDown(attachKey) /**&& instantiatedObject == null**/)
             {
+              
                 // Find the player with the "Player" tag.
-                GameObject player = GameObject.FindGameObjectWithTag(playerTag);
 
                 // Check if a player object is found.
                 if (player != null)
@@ -61,16 +75,27 @@ public class Attacher : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Check if the colliding object has the "Player" tag.
-        if (other.CompareTag(playerTag))
+        if (other.CompareTag(playerTag) || other.CompareTag(player2Tag))
         {
+            if (other.CompareTag(playerTag))
+            {
+                p1p2 = 1;
+            }
+            else if (other.CompareTag(player2Tag))
+            {
+                p1p2 = 2;
+            }
             // Check if the player already has an attached object.
             if (other.transform.childCount == 2)
             {
-                isPlayerNear = false; // Prevent attachment when an object is already attached.
+                isPlayerNear = false;
+                p1p2 = 0;
+                // Prevent attachment when an object is already attached.
             }
             else
             {
                 isPlayerNear = true;
+
             }
         }
     }
@@ -78,9 +103,11 @@ public class Attacher : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         // Check if the colliding object has the "Player" tag.
-        if (other.CompareTag(playerTag))
+        if (other.CompareTag(playerTag) || other.CompareTag(player2Tag))
         {
             isPlayerNear = false;
+            p1p2 = 0;
+
         }
     }
     
