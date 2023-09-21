a505f1fc738656b7c48d61cdd3b71cd5c980bbe5
Prakash <127179011+ProPrak01@users.noreply.github.com>
2023-09-21 23:17:26 +0530

    c

--- Assets/_Assets/Scripts/SlabInteract.cs
+++ Assets/_Assets/Scripts/SlabInteract.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
-
+using System.Collections;
 public class SlabInteract : MonoBehaviour
 {
     public string playerTag = "Player"; // Tag of the player GameObject.
     public float interactionDistance = 2.0f; // Distance to trigger interaction with the block.
     public Material glowMaterial; // Material to apply when the player is near the block.
-    public Animator animator; // Reference to the Animator component.
+    public float rotationSpeed = 45.0f; // Rotation speed in degrees per second.
     public string animationParameter = "IsActivated"; // Name of the animation parameter.
 
     private bool isPlayerNear = false; // Flag to track player proximity.
     private Renderer slabRenderer; // Reference to the slab's renderer.
     private Material originalMaterial; // Store the original material.
+    private bool isRotating = false; // Flag to track if the block is currently rotating.
+    private Quaternion startRotation; // Starting rotation of the block.
+    private Quaternion targetRotation; // Target rotation (180 degrees).
+    private Quaternion originalRotation; // Original rotation of the block.
 
     void Start()
     {
         slabRenderer = GetComponent<Renderer>();
-        originalMaterial = slabRenderer.material; 
-        animator = GetComponent<Animator>();// Store the original material.
+        originalMaterial = slabRenderer.material;
+        originalRotation = transform.rotation; // Store the original rotation.
+        startRotation = originalRotation;
+        targetRotation = Quaternion.Euler(0, 0, 180) * startRotation; // Rotate 180 degrees around the Y-axis.
     }
 
     void Update()
@@ -25,17 +31,23 @@ public class SlabInteract : MonoBehaviour
         {
             slabRenderer.material = glowMaterial;
 
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.E) && !isRotating)
             {
-                animator.SetBool(animationParameter, true);
+                // Start the rotation when the player presses "E".
+                StartCoroutine(RotateBlock(targetRotation));
             }
-
         }
         else
         {
             slabRenderer.material = originalMaterial;
-            animator.SetBool(animationParameter, false);
-
+            /**
+            // Reverse the rotation when the player gets off the block.
+            if (isRotating)
+            {
+                StopCoroutine("RotateBlock"); // Stop the ongoing rotation.
+                StartCoroutine(RotateBlock(originalRotation));
+            }
+            **/
         }
     }
 
@@ -52,8 +64,30 @@ public class SlabInteract : MonoBehaviour
         if (other.CompareTag(playerTag))
         {
             isPlayerNear = false;
-        //   animator.SetBool(animationParameter, false);
-
+            StartCoroutine(RotateBlock(originalRotation));
         }
+    }
+
+    private IEnumerator RotateBlock(Quaternion target)
+    {
+        isRotating = true;
+
+        float journeyLength = Quaternion.Angle(transform.rotation, target);
+        float journeyTime = journeyLength / rotationSpeed;
+
+        float startTime = Time.time;
+        float endTime = startTime + journeyTime;
+
+        while (Time.time < endTime)
+        {
+            float fractionOfJourney = (Time.time - startTime) / journeyTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, target, fractionOfJourney);
+            yield return null;
+        }
+
+        // Ensure the block reaches the target rotation exactly.
+        transform.rotation = target;
+
+        isRotating = false;
     }
 }
