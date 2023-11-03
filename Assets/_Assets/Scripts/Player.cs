a10186e910bd4719e84823d4c487ee18fa6d76bc
Deepak Bavge <dr.bavge60@gmail.com>
2023-11-04 03:32:43 +0530

    ch

--- Assets/_Assets/Scripts/Player.cs
+++ Assets/_Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ using static UnityEngine.ParticleSystem;
 
 public class Player : NetworkBehaviour
 {
-  //  private ParticleSystem trail;
+    //  private ParticleSystem trail;
     [SerializeField] private float moveSpeed = 10f;
     Animator animator;
     public PlayerInputAction playerControls;
@@ -19,20 +19,21 @@ public class Player : NetworkBehaviour
 
     private ulong clientId_test;
     private ulong clientId_test1;
+    private ulong passClientId_test;
 
     private void Awake()
     {
-        
+
         playerControls = new PlayerInputAction();
 
     }
     void Start()
     {
-        
-      particle = GameObject.FindGameObjectWithTag("particles");
+
+        particle = GameObject.FindGameObjectWithTag("particles");
         PlayerData playerdata = GameNetworkManager.Instance.GetPlayerDataFromPlayerIndex(0);
         PlayerData playerdata1 = GameNetworkManager.Instance.GetPlayerDataFromPlayerIndex(1);
-        
+
         clientId_test = playerdata.clientId;
         clientId_test1 = playerdata1.clientId;
         // trail = GetComponentInChildren<ParticleSystem>();
@@ -42,18 +43,19 @@ public class Player : NetworkBehaviour
         //  view = GetComponent<PhotonView>();
         if (OwnerClientId == clientId_test)
         {
-
+            passClientId_test = clientId_test1;
         }
         else if (OwnerClientId == clientId_test1)
         {
+            passClientId_test = clientId_test;
 
         }
-       
+
 
         PlayerData playerData = GameNetworkManager.Instance.GetPlayerDataFromClientId(OwnerClientId);
         playerVisual.SetPlayerColor(GameNetworkManager.Instance.GetPlayerColor(playerData.colorId));
     }
-   
+
     /*private void OnEnable()
     {
         move = playerControls.Player.Move;
@@ -66,10 +68,10 @@ public class Player : NetworkBehaviour
        // input.Disable();
     }*/
     private bool isWalking;
-    
+
     private void Update()
     {
-        
+
 
 
 
@@ -88,70 +90,90 @@ public class Player : NetworkBehaviour
 
 
         Vector2 inputVector = new Vector2(0, 0);
-        
-                if(Input.GetKey(KeyCode.W))
-                {
-                    inputVector.y = +1;
-                }
 
-                if(Input.GetKey(KeyCode.S))
-                {
-                    inputVector.y = -1;
-                }
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
 
-                if(Input.GetKey(KeyCode.D))
-                {
-                    inputVector.x = +1;
-                }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
 
-                if(Input.GetKey(KeyCode.A))
-                {
-                    inputVector.x = -1;
-                }
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+
 
         //inputVector.x = Input.GetAxis("Horizontal");
         //inputVector.y = Input.GetAxis("Vertical");
-        SendInputToServerRpc(inputVector);
+        SendInputToServerRpc(inputVector, passClientId_test);
         inputVector = inputVector.normalized;
-        
+
         //Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-      //  Vector3 movedir2 = new Vector3(inputVector.x, 0, inputVector.y);
-       // transform.position += movedir2 * moveSpeed * Time.deltaTime;
+        //  Vector3 movedir2 = new Vector3(inputVector.x, 0, inputVector.y);
+        // transform.position += movedir2 * moveSpeed * Time.deltaTime;
 
-      //  isWalking = movedir2 != Vector3.zero;
+        //  isWalking = movedir2 != Vector3.zero;
 
-     
-       // transform.forward = Vector3.Slerp(transform.forward, movedir2, rotateSpeed * Time.deltaTime);
+
+        // transform.forward = Vector3.Slerp(transform.forward, movedir2, rotateSpeed * Time.deltaTime);
 
 
         animator.SetBool("IsWalking", isWalking);
     }
     [ServerRpc(RequireOwnership = false)]
-    private void SendInputToServerRpc(Vector2 input)
+    private void SendInputToServerRpc(Vector2 input, ulong passedId)
     {
         // Apply the input on the server and synchronize it with clients
-        MovePlayerClientRpc(input);
+        MovePlayerClientRpc(input, passedId);
+
     }
+    /**
+    Vector3 moveDir = new Vector3(input.x * 1000, 0f, input.y * 1000);
+    //  transform.position += moveDir * moveSpeed * Time.deltaTime;
+    transform.position = Vector3.MoveTowards(transform.position, transform.position + moveDir, moveSpeed * Time.deltaTime);
+
+    float rotateSpeed = 10f;
+    transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+    //  float rotateSpeed = 10f;
+    // Optionally, update the player's rotation based on the movement direction
+    if (moveDir != Vector3.zero)
+    {
+        transform.rotation = Quaternion.LookRotation(moveDir);
+    }
+    **/
+
 
     [ClientRpc]
-    private void MovePlayerClientRpc(Vector2 input)
+    private void MovePlayerClientRpc(Vector2 input, ulong passedId)
     {
         // Exclude the local player (IsOwner) from movement
-        
+        if (OwnerClientId == passedId)
+        {
+
             // Apply the movement on all clients
-            Vector3 moveDir = new Vector3(input.x*1000, 0f, input.y*1000);
+            Vector3 moveDir = new Vector3(input.x, 0f, input.y);
             //  transform.position += moveDir * moveSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, transform.position + moveDir, moveSpeed * Time.deltaTime);
 
             float rotateSpeed = 10f;
             transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
-          //  float rotateSpeed = 10f;
+            //  float rotateSpeed = 10f;
             // Optionally, update the player's rotation based on the movement direction
             if (moveDir != Vector3.zero)
             {
                 transform.rotation = Quaternion.LookRotation(moveDir);
             }
-        
+        }
     }
+
+
 }
