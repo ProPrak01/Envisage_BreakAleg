a4d9177d0f538f1b96996b1d16b8c93eb232a28b
Prakash <127179011+ProPrak01@users.noreply.github.com>
2023-11-03 22:11:06 +0530

    ch

--- Assets/_Assets/Scripts/Player.cs
+++ Assets/_Assets/Scripts/Player.cs
@@ -65,43 +65,68 @@ public class Player : NetworkBehaviour
 
 
         Vector2 inputVector = new Vector2(0, 0);
+        /**
+                if(Input.GetKey(KeyCode.W))
+                {
+                    inputVector.y = +1;
+                }
 
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
+                if(Input.GetKey(KeyCode.S))
+                {
+                    inputVector.y = -1;
+                }
 
-        if(Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
+                if(Input.GetKey(KeyCode.D))
+                {
+                    inputVector.x = +1;
+                }
 
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        
+                if(Input.GetKey(KeyCode.A))
+                {
+                    inputVector.x = -1;
+                }
+        **/
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+        SendInputToServerRpc(inputVector);
         inputVector = inputVector.normalized;
         
         //Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        Vector3 movedir2 = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += movedir2 * moveSpeed * Time.deltaTime;
+      //  Vector3 movedir2 = new Vector3(inputVector.x, 0, inputVector.y);
+       // transform.position += movedir2 * moveSpeed * Time.deltaTime;
 
-        isWalking = movedir2 != Vector3.zero;
+      //  isWalking = movedir2 != Vector3.zero;
 
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, movedir2, rotateSpeed * Time.deltaTime);
+     
+       // transform.forward = Vector3.Slerp(transform.forward, movedir2, rotateSpeed * Time.deltaTime);
 
 
         animator.SetBool("IsWalking", isWalking);
     }
+    [ServerRpc(RequireOwnership = false)]
+    private void SendInputToServerRpc(Vector2 input)
+    {
+        // Apply the input on the server and synchronize it with clients
+        MovePlayerClientRpc(input);
+    }
 
-
-
+    [ClientRpc]
+    private void MovePlayerClientRpc(Vector2 input)
+    {
+        // Exclude the local player (IsOwner) from movement
+        if (!IsOwner)
+        {
+            // Apply the movement on all clients
+            Vector3 moveDir = new Vector3(input.x, 0f, input.y);
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+          //  float rotateSpeed = 10f;
+            // Optionally, update the player's rotation based on the movement direction
+            if (moveDir != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(moveDir);
+            }
+        }
+    }
 }
